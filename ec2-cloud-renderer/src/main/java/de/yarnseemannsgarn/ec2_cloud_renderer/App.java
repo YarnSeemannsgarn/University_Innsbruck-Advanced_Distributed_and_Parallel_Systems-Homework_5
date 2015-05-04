f2bfdf493e088ca5eadb7c2226ca1833a75ba378
YarnSeemannsgarn <janschlenker1990@gmail.com>
2015-05-04 15:10:24 +0200

    Change maven to quickstart archetype

---
+++ ec2-cloud-renderer/src/main/java/de/yarnseemannsgarn/ec2_cloud_renderer/App.java
@@ -0,0 +1,13 @@
+package de.yarnseemannsgarn.ec2_cloud_renderer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
